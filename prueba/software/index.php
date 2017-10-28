6720e7d1114e276b793485739f4e3a1af6bc556d
Juan Callisaya <arteagaj303@gmail.com>
2017-10-28 19:52:39 +0200

    mi primer menu

---
+++ prueba/software/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>ANHELOS</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="container">
+	  <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills float-right">
+            <li class="nav-item">
+              <a class="nav-link active" href="index.html">Laravel</a>
+            </li>
+            <li>
+              <a class="nav-link" href="php.html">Php</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="git.html">Git</a>
+            </li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Anhelos Virtuales</h3>
+      </div>
+
+		<div class="text-center">
+		<header>
+			<h1>Curso Laravel 5.4</h1>
+		</header>
+			<img class="img-responsive" src="images/laravel.png" alt="">
+			<hr/>
+			<ul class="list-group">
+			  <li class="list-group-item disabled">Desarrollo de Aplicaciones y Servicios Web con Laravel 5.4</li>
+			  <li class="list-group-item">Del 30 de octubre al 04 de noviembre de 2017</li>
+			  <li class="list-group-item">Inscripciones: Secretaria Academia CISCO - Oruro</li>
+			  <li class="list-group-item">Costo 200 Bs.</li>
+			</ul>
+		<div>
+		<footer class="footer">
+			<p>&copy; Saul Mamani <M class=""></M></p>
+		</footer>
+	</div>
+</body>
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
